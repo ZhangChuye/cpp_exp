3c857b729ea6ee96a3db0178ec0b9304f135f848
20chase <tingxiangfan@gmail.com>
2023-07-31 22:13:59 +0800

    download other's tool

---
+++ kitti2pcl/build/CMakeFiles/velo2pcd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/velo2pcd.dir/velodyne2pcd.cpp.o"
+  "velo2pcd"
+  "velo2pcd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/velo2pcd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
